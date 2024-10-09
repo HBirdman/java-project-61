02a52dd92ed9e6f14af25b29e4c137c3a45fdcce
HBirdman <hbirdman@mail.ru>
2024-10-09 21:58:00 +0300

    Cli class improvements

--- app/src/main/java/hexlet/code/Cli.java
+++ app/src/main/java/hexlet/code/Cli.java
@@ -1,8 +1,15 @@
 package hexlet.code;
 
+import java.util.Scanner;
+
 public class Cli {
     public static void greetings() {
         var greeting = "Welcome to the Brain Games!";
         System.out.println(greeting);
+        Scanner sc = new Scanner(System.in);
+        System.out.println("May I have your name?");
+        String name = sc.nextLine();
+        System.out.println("Hello, " + name + "!");
+        sc.close();
     }
 }
